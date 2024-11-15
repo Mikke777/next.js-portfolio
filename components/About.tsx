3e3f2a6b91d5e56acca3c9020bacb7c71ce0afcf
michele di stadio <distadiomichele93@gmail.com>
2024-11-15 16:09:35 +0100

    layout of about

---
+++ components/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BentoGridSecondDemo } from './BentoGridSecondDemo'
+
+const About = () => {
+  return (
+    <section id="about">
+      <BentoGridSecondDemo />
+    </section>
+  )
+}
+
+export default About
