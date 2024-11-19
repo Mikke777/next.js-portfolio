1e7fd50c2a2f2150ad339b2fe2a675dfc6b9bde5
michele di stadio <distadiomichele93@gmail.com>
2024-11-19 20:03:07 +0100

    carousel

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,61 +1,55 @@
-import { projects } from '@/data'
-import React from 'react'
-import { PinContainer } from './ui/3d-pin'
+"use client";
+import Image from "next/image";
+import React from "react";
+import { Carousel, Card } from "@/components/ui/apple-cards-carousel";
+import { data as projectData } from "@/data/index";
 
-const Project = () => {
+const DummyContent = ({ content }) => {
   return (
-    <div className='py-20' suppressHydrationWarning>
-      <h1 className='heading'>
-        Some of my Projects
-      </h1>
-      <div className='flex flex-row items-center justify-center p-4 gap-16 mt-10'>
-        {projects.map(({ id, title, des, img, iconLists, link }) => (
-          <div
-            key={id}
-            className='lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w:96 w-[80vw]'
-          >
-            <PinContainer
-            title={title}
-            href={link}
-            >
-              <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10'>
-                {/* <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
-                  <img src='/bg.png' alt='bg-png' />
-                </div> */}
-                <img
-                  src={img}
-                  alt={title}
-                  className='z-10 absolute bottom-0 h-60'
-                />
-              </div>
-
-              <h1 className='font-bold lg:text-2xl md-text-xl text-base line-clamp-1'>
-                {title}
-              </h1>
-
-              <p className='lg:text-xl lg:font-normal font-light text-sm line-clamp-2'>
-                {des}
-              </p>
-
-              <div className='flex items-center justify-between mt-7 mb-3'>
-                <div className='flex items-center'>
-                  {iconLists.map((icon, index) => (
-                    <div
-                      key={icon}
-                      className='border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center'
-                      style={{ transform: `translateX(-${5 * index * 2}px)` }}
-                    >
-                      <img src={icon} alt={icon} className='p-2' />
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </PinContainer>
+    <>
+      {content.map((item, index) => (
+        <div
+          key={index}
+          className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4"
+        >
+          <div className="mt-4 mb-4">
+            <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+              <span className="font-bold text-neutral-700 dark:text-neutral-200">
+                {item.title}
+              </span>{" "}
+              {item.description}
+            </p>
+            <Image
+              src={item.imgSrc}
+              alt={item.imgAlt}
+              height={500}
+              width={500}
+              className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+            />
           </div>
-        ))}
-      </div>
-    </div>
-  )
-}
+        </div>
+      ))}
+    </>
+  );
+};
 
-export default Project
+
+const contentMap = projectData.reduce((acc, item) => {
+  acc[item.contentId] = <DummyContent content={item.content} />;
+  return acc;
+}, {});
+
+export function AppleCardsCarouselDemo() {
+  const cards = projectData.map((card, index) => (
+    <Card key={card.src} card={{ ...card, content: contentMap[card.contentId] }} index={index} />
+  ));
+
+  return (
+    <div className="w-full h-full py-20">
+      <h2 className="max-w-7xl pl-4 mx-auto text-xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200 font-sans">
+        Some of my projects!
+      </h2>
+      <Carousel items={cards} />
+    </div>
+  );
+}
