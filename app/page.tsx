486e56720b6f4bec45dd7f87fe76fb2963bc4f4a
michele di stadio <distadiomichele93@gmail.com>
2024-11-21 17:06:33 +0100

    test7

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-// import About from "@/components/About";
-// import Hero from "@/components/Hero";
-// import { ModeToggle } from "@/components/ModeToggle";
-// import { AppleCardsCarouselDemo } from "@/components/Project";
-// import { FloatingNav } from "@/components/ui/FloatingNav";
-// import { FaHome } from "react-icons/fa";
-// import { FcAbout } from "react-icons/fc";
-// import { GrProjects } from "react-icons/gr";
 import Footer from "@/components/Footer";
 
 export default function Home() {
@@ -14,15 +6,6 @@ export default function Home() {
     justify-center items-center flex-col overflow-hidden
     mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        {/* <FloatingNav navItems={[
-          { name: "Home", link: "/", icon: <FaHome /> },
-          { name: "About", link: "#about", icon: <FcAbout /> },
-          { name: "Projects", link: "#AppleCardsCarouselDemo", icon: <GrProjects /> },
-        ]} /> */}
-        {/* <Hero /> */}
-        {/* <ModeToggle /> */}
-        {/* <About /> */}
-        {/* <AppleCardsCarouselDemo /> */}
         <Footer />
       </div>
     </main>
