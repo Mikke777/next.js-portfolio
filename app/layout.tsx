e70186d260c2a4185d8f92fb5cfc8ef5996383f6
michele di stadio <distadiomichele93@gmail.com>
2024-11-21 18:27:42 +0100

    test8

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,53 +1,25 @@
-"use client";
-
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "./theme-provider";
-import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Adrian's Portfolio",
+  description: "Modern & Minimal JS Mastery Portfolio",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [theme, setTheme] = useState(() => {
-    if (typeof window !== "undefined") {
-      const savedTheme = localStorage.getItem("theme");
-      if (savedTheme) {
-        return savedTheme;
-      } else {
-        const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
-        return prefersDarkScheme ? "dark" : "light";
-      }
-    }
-    return "light";
-  });
-
-  useEffect(() => {
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme) {
-      setTheme(savedTheme);
-    } else {
-      const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: light)").matches;
-      const initialTheme = prefersDarkScheme ? "dark" : "light";
-      setTheme(initialTheme);
-      localStorage.setItem("theme", initialTheme);
-    }
-  }, []);
-
   return (
-    <html lang="en" className={theme} style={{ colorScheme: theme }} suppressHydrationWarning>
-      <body className={inter.className} suppressHydrationWarning>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+    <html lang="en" suppressHydrationWarning>
+      <head>
+      </head>
+      <body className={inter.className}>
+        {children}
       </body>
     </html>
   );
--- app/metadata.ts
+++
@@ -1,6 +0,0 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Michele Di Stadio Portfolio",
-  description: "Modern and minimalist portfolio of Michele Di Stadio",
-};
