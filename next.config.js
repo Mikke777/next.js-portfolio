42e76f97f01f359c3cd00ccf19ed1b7ee7c8ac84
michele di stadio <distadiomichele93@gmail.com>
2024-11-21 15:32:09 +0100

    test 3

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  staticPageGenerationTimeout: 300,
-  experimental: {
-    instrumentationHook: true,
-  },
 };
 
 module.exports = nextConfig;
